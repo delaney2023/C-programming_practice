0c809d6c04ffdb45d948d31886671c764c6af3f8
delaney2023 <alxse2@gmail.com>
2023-09-06 22:33:47 -0700

    quiz

---
+++ variables-if_else/1-programiz_quiz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - display the output of the variable
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int number1 = 33;
+
+	printf("%d", number1);
+	printf("%d\n", number1);
+	return (0);
+}
