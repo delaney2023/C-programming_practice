96d7d2fd98f3e99857046e117c8e90fdbbd8fab1
delaney2023 <alxse2@gmail.com>
2023-09-06 21:52:35 -0700

    creating a variable and storing data in it

---
+++ variables-if_else/0-variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description - declares a variable
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int age =25;
+
+	printf("Age: %d\n", age);
+	return (0);
+}
